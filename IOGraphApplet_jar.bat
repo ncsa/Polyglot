953aadd1563425009ff3056ead06ac5cc5c6e346
Kenton McHenry <kmchenry@ncsa.illinois.edu>
2010-03-06 04:38:36 +0000

    Began switch to ant builds.
    git-svn-id: file:///home/kooper/svn2git/polyglot/trunk@42 441973dc-d2ab-436a-baab-01a36545c698

--- IOGraphApplet_jar.bat
+++ IOGraphApplet_jar.bat
@@ -1,13 +1,12 @@
-mkdir tmp\snapshot\IOGraphApplet_jar\edu\ncsa\icr\polyglot
-xcopy bin\edu\ncsa\icr\polyglot\IOGraph*.class tmp\snapshot\IOGraphApplet_jar\edu\ncsa\icr\polyglot /i /y
-xcopy bin\edu\ncsa\icr\polyglot\Conversion*.class tmp\snapshot\IOGraphApplet_jar\edu\ncsa\icr\polyglot /i /y
-xcopy bin\edu\ncsa\icr\polyglot\Point2D*.class tmp\snapshot\IOGraphApplet_jar\edu\ncsa\icr\polyglot /i /y
-xcopy bin\edu\ncsa\icr\polyglot\FadedScrollBarUI*.class tmp\snapshot\IOGraphApplet_jar\edu\ncsa\icr\polyglot /i /y
+mkdir build\IOGraphApplet\edu\ncsa\icr\polyglot
+xcopy bin\edu\ncsa\icr\polyglot\IOGraph*.class build\IOGraphApplet\edu\ncsa\icr\polyglot /i /y
+xcopy bin\edu\ncsa\icr\polyglot\Conversion*.class build\IOGraphApplet\edu\ncsa\icr\polyglot /i /y
+xcopy bin\edu\ncsa\icr\polyglot\Point2D*.class build\IOGraphApplet\edu\ncsa\icr\polyglot /i /y
+xcopy bin\edu\ncsa\icr\polyglot\FadedScrollBarUI*.class build\IOGraphApplet\edu\ncsa\icr\polyglot /i /y
 
-cd tmp/snapshot/IOGraphApplet_jar
-jar xf ../../../lib/ncsa/Utilities.jar
-REM jar xf ../../../lib/mysql-connector-java-5.1.10/mysql-connector-java-5.1.10-bin.jar
-cd ../../..
+cd build/IOGraphApplet
+jar xf ../../lib/ncsa/Utilities.jar
+cd ../..
 
-jar cf tmp/snapshot/IOGraphApplet.jar -C tmp/snapshot/IOGraphApplet_jar .
-jarsigner -signedjar tmp/snapshot/IOGraphApplet-signed.jar tmp/snapshot/IOGraphApplet.jar mykey
+jar cf build/IOGraphApplet.jar -C build/IOGraphApplet .
+jarsigner -signedjar build/IOGraphApplet-signed.jar build/IOGraphApplet.jar mykey
