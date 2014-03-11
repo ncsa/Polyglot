a16ccc7d051897cd6de9fba0294b0f98c853eeac
Edgar F. Black <efblack2@illinois.edu>
2014-03-11 15:39:01 -0500

    video converter

--- scripts/sh/Avconv_convert.sh
+++ scripts/sh/Avconv_convert.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-#soundconverter (2.0.4)
-#sound
+#avconv (0.8.9-6)
+#video
 #mp4, ogg, wmv, mpeg, flv, ogv, mkv, vob, asf
 #ogg, wmv, mpeg, flv, ogv, mkv, vob, asf
-
-#echo $1
-#echo $2
 
 avconv -i "$1" "$2"
