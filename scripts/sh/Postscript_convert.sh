a2356fdde026bbcab6b7d486ac8b330017027395
Edgar F. Black <efblack2@illinois.edu>
2014-03-12 10:40:02 -0500

    revision

--- scripts/sh/Postscript_convert.sh
+++ scripts/sh/Postscript_convert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# poscript2Many
-# documents
-# ps
-# ascii, eps, epsi, pdf, pdf12, pdf13, pdf14, pdfwr, ps, ps2, txt
+#Poscript2Many
+#documents
+#ps
+#ascii, eps, epsi, pdf, pdf12, pdf13, pdf14, pdfwr, ps, ps2, txt
 
 #inputFile=`echo ${1##*/}`
 #outputFile=`echo ${2##*/}`
@@ -11,25 +11,27 @@ extO=`echo ${2##*.}`
 
 
 if [ "pdf" = "$extO" ]; then
-    ps2pdf $1 $2
+    ps2pdf "$1" "$2"
 elif [ "pdf12" = "$extO" ]; then
-    ps2pdf12 $1 $2
+    ps2pdf12 "$1" "$2"
 elif [ "pdf13" = "$extO" ]; then
-    ps2pdf13 $1 $2
+    ps2pdf13 "$1" "$2"
 elif [ "pdf14" = "$extO" ]; then
-    ps2pdf14 $1 $2
+    ps2pdf14 "$1" "$2"
 elif [ "ascii" = "$extO" ]; then
-    ps2ascii $1 $2
+    ps2ascii "$1" "$2"
 elif [ "txt" = "$extO" ]; then
-    ps2txt $1 $2
+    ps2txt "$1" "$2"
 elif [ "eps" = "$extO" ]; then
-    ps2eps $1
+    ps2eps -f "$1"
 elif [ "epsi" = "$extO" ]; then
-    ps2epsi $1
+    ps2epsi "$1" "$2"
 elif [ "ps" = "$extO" ]; then
-    ps2ps $1 $2.ps
+    cp "$1" ./temp.ps
+    ps2ps ./temp.ps "$2"
+    rm ./temp.ps
 elif [ "ps2" = "$extO" ]; then
-    ps2ps2 $1 $2
+    ps2ps2 "$1" "$2"
 elif [ "pdfwr" = "$extO" ]; then
-    ps2pdfwr $1 $2
+    ps2pdfwr "$1" "$2"
 fi
