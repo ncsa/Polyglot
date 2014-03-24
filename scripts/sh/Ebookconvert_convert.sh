57d8f18d138b831b1c30c8a351cd601631966c2c
Edgar F. Black <efblack2@illinois.edu>
2014-03-24 08:36:08 -0500

    updated to version 1.29

--- scripts/sh/Ebookconvert_convert.sh
+++ scripts/sh/Ebookconvert_convert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Ebookconvert (v1.25)
+#Ebookconvert (v1.29)
 #ebook conversions
 #cbz, cbr, cbc, chm, djvu, docx, epub, fb2, html, htmlz, lit, lrf, mobi, odt, pdf, prc, pdb, pml, rb, rtf, snb, tcr, txt, txtz
 #azw3, epub, fb2, oeb, lit, lrf, mobi, htmlz, pdb, pml, rb, pdf, rtf, snb, tcr, txt, txtz
