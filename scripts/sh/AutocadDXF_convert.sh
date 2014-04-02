7321d1e0114cc88b49ba73c7c45212b92ef7c00c
Edgar F. Black <efblack2@illinois.edu>
2014-04-02 11:12:15 -0500

    Convert autocad dxf (ascii) to other formats

---
+++ scripts/sh/AutocadDXF_convert.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Kabeja (v0.4)
+# Convert autocad dxf (ascii) to other formats.
+#dxf
+#svg, pdf, png, tif, tiff
+
+inputFile=$(basename "$1")
+#echo $inputFile
+
+outputFile=$(basename "$2")
+#echo $outputFile
+
+extO=${outputFile##*.}
+#echo $extO
+if [ "$extO" == "tiff" ]; then
+    outputFile=${outputFile%?}
+    #echo "after removal of last char: "$outputFile
+elif [ "$extO" == "tif" ]; then
+    extO="tiff"
+    #echo "adding character: "$extO
+fi
+
+cd /opt/kabeja-0.4/
+cp "$1" "./$inputFile"
+
+kabeja.sh  -nogui -pp conf/process.xml   -pipeline "$extO"   "$inputFile" "$2"
+rm "$inputFile"
+#mv "$outputFile" "$2"
+
