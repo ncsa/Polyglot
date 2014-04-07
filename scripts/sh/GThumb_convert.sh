129aa34921fb30f74c4691cf64c944201f35ffb2
Edgar F. Black <efblack2@illinois.edu>
2014-04-07 14:59:46 -0500

    gthumb View and organize your images

---
+++ scripts/sh/GThumb_convert.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#GThumb (v3.0.2)
+#images
+#jpeg,jpg, tiff,tif, png, tga
+#jpeg,jpg, tiff,tif, png, tga
+
+## This script need some debug.
+## It does not write png files
+## correctly when run from poliglot.
+
+inputFile=`echo ${1##*/}`
+outputFile=`echo ${2##*/}`
+#echo $inputFile
+#echo $outputFile
+
+xdotool exec --sync   gthumb "$1"  2> /dev/null  &
+sleep 3             ## waitting for file to open
+xdotool search --name $inputFile.".*gThumb" windowactivate --sync  
+
+sleep 1
+xdotool key  --clearmodifiers  "alt+f" 
+xdotool key  --clearmodifiers  "a" 
+xdotool key  --clearmodifiers  "Return" 
+
+sleep 1
+xdotool search --name "Save Image" windowactivate --sync  
+sleep 1
+xdotool key --clearmodifiers 'Home+shift+End+Delete'
+xdotool type "$2"
+sleep 1
+xdotool search --name "Save Image" windowactivate --sync  
+xdotool key --clearmodifiers 'Return'
+sleep 3
+xdotool search --name $outputFile.".*gThumb" windowactivate --sync  
+sleep 1
+xdotool key --clearmodifiers 'ctrl+q'
