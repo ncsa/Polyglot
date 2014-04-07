c952f1c17926d49333926bc99f372310d6d42433
Edgar F. Black <efblack2@illinois.edu>
2014-04-07 11:16:40 -0500

    ImageViewer (eog). Does not write png files. need debug

---
+++ scripts/sh/ImageViewer_convert.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#Image Viewer (v3.6.2)
+#images
+#jpeg, bmp, tiff, png, ico
+#jpeg, bmp, tiff, png, ico
+
+## This script need some debug.
+## It does not write png files
+## correctly.
+
+inputFile=`echo ${1##*/}`
+#echo $inputFile
+#echo $2
+
+xdotool exec --sync   eog "$1"  2> /dev/null  "$1" &
+sleep 3             ## waitting for file to open
+xdotool search --name $inputFile windowactivate --sync  
+sleep 1
+xdotool key  --clearmodifiers  "ctrl+shift+s" 
+sleep 1
+xdotool search --name "Save Image" windowactivate --sync  
+sleep 1
+xdotool key --clearmodifiers 'Home+shift+End+Delete'
+xdotool type "$2"
+sleep 1
+xdotool search --name "Save Image" windowactivate --sync  
+xdotool key --clearmodifiers 'Return'
+sleep 1
+xdotool search --name $inputFile windowactivate --sync  
+sleep 1
+xdotool key --clearmodifiers 'ctrl+w'
