71ae6e3f4bb534551bb435b85951d610d8ebe449
Edgar F. Black <efblack2@illinois.edu>
2014-04-10 11:03:50 -0500

    added docx expension

--- scripts/sh/Unoconv_convert.sh
+++ scripts/sh/Unoconv_convert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#Unoconv (LibreOffice 4.0)
+#Unoconv (v 0.5)
 #Dociments conversions
-#doc, html, odp, ods, odt, ppt, rtf, sdw, sxw, txt, xls, xlsx, xlsm, xml
-#odt, pdf, ppt, doc, html, rtf, sxw, txt, xml, odp, ods, xls, xlsx
+#doc, docx, html, odp, ods, odt, ppt, rtf, sdw, sxw, txt, xls, xlsx, xlsm, xml
+#odt, pdf, ppt, doc, docx, html, rtf, sxw, txt, xml, odp, ods, xls, xlsx
 
-extO=`echo ${2##*.}`
+extO="${2##*.}"
 
 unoconv   -f $extO  -o "$2"   "$1"
