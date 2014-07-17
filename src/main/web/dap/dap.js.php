a36200e83e548e995b84c1daa0da382bfb601288
Kenton McHenry <kmchenry@ncsa.illinois.edu>
2014-07-16 21:39:58 -0500

    Added graphic to javascript for inline html calls to the DAP.

--- src/main/web/dap/dap.js.php
+++ src/main/web/dap/dap.js.php
@@ -1,5 +1,5 @@
 //Set DAP server
-var dap = "http://" + "<?php echo $_SERVER['SERVER_NAME']; ?>" + ":8184/convert/";
+var dap = "http://" + "<?php echo $_SERVER['SERVER_NAME']; ?>";
 
 //Load JQuery
 if (!window.jQuery) {
@@ -11,7 +11,49 @@ if (!window.jQuery) {
 
 //Process webpage once loaded
 window.onload = function() {
+	addGraphic();
+
 	$("a[dap]").each(function() {
-		this.href = dap + $(this).attr('dap') + "/" + encodeURIComponent(this.href);
+		this.href = dap + ":8184/convert/" + $(this).attr('dap') + "/" + encodeURIComponent(this.href);
 	});
 };
+
+//Brown Dog graphic
+function addGraphic() {
+	var graphic = $('<img>')
+		.attr('src', dap + '/dap/images/browndog-small.gif')
+		.attr('width', '25')
+		.attr('id', 'graphic')
+		.css('position', 'absolute')
+		.css('left', '0px')
+		.css('bottom', '25px')
+	$("body").append(graphic);
+
+	setTimeout(moveGraphicRight, 10);
+}
+
+function moveGraphicRight() {
+	var graphic = document.getElementById('graphic');
+	graphic.style.left = parseInt(graphic.style.left) + 25 + 'px';
+
+	if(parseInt(graphic.style.left) < $(window).width() - 50) {
+		setTimeout(moveGraphicRight, 10);
+	} else {
+		graphic.remove();
+
+		//Add powered by graphic
+		graphic = $('<img>')
+			.attr('src', dap + '/dap/images/poweredby.gif')
+			.attr('width', '100');
+
+		var link = $('<a/>')
+			.attr('href', 'http://browndog.ncsa.illinois.edu')
+			.attr('id', 'poweredby')
+			.css('position', 'fixed')
+			.css('right', '10px')
+			.css('bottom', '10px')
+			.append(graphic);
+
+		$("body").append(link);
+	}
+}
--- src/main/web/dap/publish.sh
+++ src/main/web/dap/publish.sh
@@ -2,3 +2,8 @@
 
 mkdir -p /var/www/dap
 cp *.php /var/www/dap
+
+mkdir -p /var/www/dap/images
+cp ../../../../images/favicon.png /var/www/dap/images 
+cp ../../../../images/browndog-small.gif /var/www/dap/images 
+cp ../../../../images/poweredby.gif /var/www/dap/images 
