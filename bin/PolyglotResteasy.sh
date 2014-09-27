1e936bf57234d6985eb98be241e4ed2ce782dbca
Edgar F. Black <efblack2@illinois.edu>
2014-09-27 08:55:57 -0500

    problem with incomplete information on /META-INF/services/javax.ws.rs.ext.Providers solved. Scripts modified accordingly

--- bin/PolyglotResteasy.sh
+++ bin/PolyglotResteasy.sh
@@ -1,1 +1,1 @@
-java -cp target/polyglot-2.1.0-SNAPSHOT-jar-with-dependencies.jar:resteasy-multipart-provider-3.0.7.Final.jar:lib/kgm/* -Xmx1g edu.illinois.ncsa.isda.softwareserver.polyglot.PolyglotResteasy
+java -cp target/polyglot-2.1.0-SNAPSHOT.jar:lib/kgm/* -Xmx1g edu.illinois.ncsa.isda.softwareserver.polyglot.PolyglotResteasy
