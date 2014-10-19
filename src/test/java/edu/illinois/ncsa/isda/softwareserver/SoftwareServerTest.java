b3b1f35c893f00789e80e5920c9025eac10d7f4e
kmchenry <mchenry@illinois.edu>
2014-10-19 10:58:27 -0500

    Cleaned up integration tests.

--- src/test/java/edu/illinois/ncsa/isda/softwareserver/SoftwareServerTest.java
+++ src/test/java/edu/illinois/ncsa/isda/softwareserver/SoftwareServerTest.java
@@ -1,9 +1,8 @@
 package edu.illinois.ncsa.isda.softwareserver;
 import edu.illinois.ncsa.isda.softwareserver.SoftwareServerAuxiliary.*;
 import kgm.utility.Utility;
-import org.junit.Test;
+import org.junit.*;
 import static org.junit.Assert.*;
-import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
 import org.restlet.data.MediaType;
 import org.restlet.representation.*;
@@ -16,57 +15,84 @@ import java.util.*;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SoftwareServerTest
 {
+	private static String script_name = "ImageMagick_convert.ahk";
+	private static SoftwareServerRestlet restlet = null;
+	private static DistributedSoftwareServerRestlet drestlet = null;
+	private static SoftwareServerClient client = null;
+
 	/**
-	 * Test ScriptInstaller.
-	 */	
+	 * Test ScriptInstaller and start up services/connections.
+	 */
+	@BeforeClass
+	public static void setup()
+	{		
+		//Configure wrapper script
+		if(System.getProperty("os.name").contains("Windows")){		//Only run the test on Windows machines!
+			ScriptDebugger debugger = new ScriptDebugger("ScriptDebugger.conf");
+			debugger.configureScript("scripts/ahk/" + script_name);
+		}
+		
+		//Start the services/connections	
+		System.out.println("\n--- Starting services/connections ---");
+		
+	  restlet = new SoftwareServerRestlet();
+		SoftwareServerRestlet.main(new String[0]);
+		
+		drestlet = new DistributedSoftwareServerRestlet();
+		drestlet.main(new String[0]);
+		
+		client = new SoftwareServerClient("localhost", 50000);;
+
+	}
+	
+	/**
+	 * Test configuration and basic connection.
+	 */
 	@Test
 	public void test1()
 	{
-		System.out.println("\n=== Software Server: Test 1 ===");
+		System.out.println("\n=== Software Server: Test 1 (configuration and connection) ===");
 		
-		if(System.getProperty("os.name").contains("Windows")){		//Only run the test on Windows machines!
-			String script_name = "ImageMagick_convert.ahk";
-			ScriptDebugger debugger = new ScriptDebugger("ScriptDebugger.conf");
-			debugger.configureScript("scripts/ahk/" + script_name);
-			
-			assertTrue("Script not configured", Utility.existsNotEmptyAndRecent("scripts/ahk-configured/" + script_name, 10000));
+		//Check script configuration
+		try{
+			assertTrue("Error: Script not configured", Utility.existsNotEmptyAndRecent("scripts/ahk-configured/" + script_name, 10000));
+			System.out.println("Script configuration OK");
+		}catch(AssertionError e){
+			System.out.println(e.getMessage());
+		}
+				
+		//Check Software Server client connection		
+		try{
+			assertTrue("Error: SoftwareServerClient not connected", client.isAlive());
+			System.out.println("SoftwareServerClient connection OK");
+		}catch(AssertionError e){
+			System.out.println(e.getMessage());
 		}
 	}
 	
 	/**
-	 * Test SoftwareServer connection.
+	 * Check SoftwareServer applications.
 	 */	
 	@Test
 	public void test2()
 	{    
-		SoftwareServer server = null;
-		SoftwareServerClient client = null;
-		
-		System.out.println("\n=== Software Server: Test 2 ===");
-
-		try {
-			//Start the Software Server
-	  	server = new SoftwareServer("SoftwareServer.conf");
-			client = new SoftwareServerClient("localhost", 50000);
-	    
-	    //Run the tests
-	    assertTrue("SoftwareServerClient not connected", client.isAlive());
-	    
-	    Vector<Application> applications = client.getApplications();
-	    boolean FOUND_IMAGEMAGICK = false;
-	    
-	    for(int i=0; i<applications.size(); i++){
-	    	if(applications.get(i).alias.equals("ImageMagick")){
-	    		FOUND_IMAGEMAGICK = true;
-	    		break;
-	    	}
-	    }
-	    
-	    assertTrue("Did not find ImageMagick", FOUND_IMAGEMAGICK);
-		} finally {
-	    //Stop the Software Server
-	    if (client != null) client.close();
-	    if (server != null) server.stop();
+		System.out.println("\n=== Software Server: Test 2 (applications) ===");
+	  
+    Vector<Application> applications = client.getApplications();
+    boolean FOUND_IMAGEMAGICK = false;
+    
+    for(int i=0; i<applications.size(); i++){
+    	if(applications.get(i).alias.equals("ImageMagick")){
+    		FOUND_IMAGEMAGICK = true;
+    		break;
+    	}
+    }
+    
+    try{
+    	assertTrue("Error: Did not find ImageMagick", FOUND_IMAGEMAGICK);
+    	System.out.println("ImageMagick OK");
+    }catch(AssertionError e){
+			System.out.println(e.getMessage());
 		}
 	}
 	
@@ -76,29 +102,19 @@ public class SoftwareServerTest
 	@Test
 	public void test3()
 	{  		
-		SoftwareServer server = null;
-		SoftwareServerClient client = null;
+		System.out.println("\n=== Software Server: Test 3 (task) ===");
 
-		System.out.println("\n=== Software Server: Test 3 ===");
-
-		try {
-			//Start the Software Server
-		  server = new SoftwareServer("SoftwareServer.conf");
-	    client = new SoftwareServerClient("localhost", 50000);
-	    
-	    //Run the test
-	    client.sendData(new FileData("data/demo/Lenna.png", true));
-			
-			Task task = new Task(client);
-			task.add("ImageMagick", "convert", "data/demo/Lenna.png", "Lenna.jpg");
-			task.execute("data/tmp/");
-			
-			assertTrue("Conversion failed", Utility.existsNotEmptyAndRecent("data/tmp/Lenna.jpg", 10000));
-			
-		} finally {
-	    //Stop the Software Server
-	    if (client != null) client.close();
-	    if (server != null) server.stop();
+    client.sendData(new FileData("data/demo/Lenna.png", true));
+		
+		Task task = new Task(client);
+		task.add("ImageMagick", "convert", "data/demo/Lenna.png", "Lenna.jpg");
+		task.execute("data/tmp/");
+		
+		try{
+			assertTrue("Error: Conversion failed", Utility.existsNotEmptyAndRecent("data/tmp/Lenna.jpg", 10000));
+			System.out.println("Conversion task OK");
+		}catch(AssertionError e){
+			System.out.println(e.getMessage());
 		}
 	}
 	
@@ -107,28 +123,29 @@ public class SoftwareServerTest
 	 */	
 	@Test
 	public void test4()
-	{  		
-		SoftwareServerRestlet server = null;
+	{  				
+		System.out.println("\n=== Software Server: Test 4 (REST interface) ===");
+	
+		String result = Utility.postFile("http://localhost:8182/software/ImageMagick/convert/jpg/", "data/demo/Lenna.png", "text/plain");
+		assertNotNull("result is null", result);
+		Utility.pause(2000);
 		
-		System.out.println("\n=== Software Server: Test 4 ===");
-
-		try {
-			//Start the Software Server and its REST interface
-		  server = new SoftwareServerRestlet();
-			SoftwareServerRestlet.main(new String[0]);
-			
-			//Run the test
-			String result = Utility.postFile("http://localhost:8182/software/ImageMagick/convert/jpg/", "data/demo/Lenna.png", "text/plain");
-			assertNotNull("result is null", result);
-			Utility.pause(2000);
-			
-			assertTrue("Did not get answer from URL [" + result + "]", Utility.existsURL(result));
+		//Check if the desired endpoint, and thus the server, is active
+		try{
+			assertTrue("Error: Did not get answer from URL [" + result + "]", Utility.existsURL(result));
+			System.out.println("Endpoint OK");
+		}catch(AssertionError e){
+			System.out.println(e.getMessage());
+		}
+	
+		//Carry out the conversion
+		Utility.downloadFile("data/tmp/", "Lenna4", result);
 		
-			Utility.downloadFile("data/tmp/", "Lenna4", result);
-			assertTrue("Conversion failed", Utility.existsNotEmptyAndRecent("data/tmp/Lenna4.jpg", 10000));
-		} finally {
-			//Stop the REST interface and its underlying Software Server
-			if (server != null) server.stop();
+		try{
+			assertTrue("Error: Conversion failed", Utility.existsNotEmptyAndRecent("data/tmp/Lenna4.jpg", 10000));
+			System.out.println("Conversion OK");
+		}catch(AssertionError e){
+			System.out.println(e.getMessage());
 		}
 	}
 	
@@ -138,33 +155,42 @@ public class SoftwareServerTest
 	@Test
 	public void test5()
 	{  		
-		SoftwareServerRestlet sserver = null;
-		DistributedSoftwareServerRestlet dsserver = null;
-				
-		System.out.println("\n=== Software Server: Test 5 ===");
+		System.out.println("\n=== Software Server: Test 5 (distributed restlet) ===");
 
-		try {
-			//Start services
-		  sserver = new SoftwareServerRestlet();
-			sserver.main(new String[0]);
-			dsserver = new DistributedSoftwareServerRestlet();
-			dsserver.main(new String[0]);
-	
-			//Run the test
-			Utility.pause(2000);
-			String result = Utility.postFile("http://localhost:8183/software/ImageMagick/convert/jpg/", "data/demo/Lenna.png", "text/plain");
-			assertNotNull("result is null", result);
-			Utility.pause(2000);
-			
-			assertTrue("Did not get answer from URL [" + result + "]", Utility.existsURL(result));
-					
-			Utility.downloadFile("data/tmp/", "Lenna5", result);
-			assertTrue("Conversion failed", Utility.existsNotEmptyAndRecent("data/tmp/Lenna5.jpg", 10000));
+		Utility.pause(2000);
+		String result = Utility.postFile("http://localhost:8183/software/ImageMagick/convert/jpg/", "data/demo/Lenna.png", "text/plain");
+		assertNotNull("result is null", result);
+		Utility.pause(2000);
 		
-		} finally {
-			//Stop the services
-			if (dsserver != null) dsserver.stop();		
-			if (sserver != null) sserver.stop();
+		//Check if the desired endpoint, and thus the server, is active
+		try{
+			assertTrue("Error: Did not get answer from URL [" + result + "]", Utility.existsURL(result));
+			System.out.println("Endpoint OK");
+		}catch(AssertionError e){
+			System.out.println(e.getMessage());
 		}
+				
+		//Carry out the conversion
+		Utility.downloadFile("data/tmp/", "Lenna5", result);
+		
+		try{
+			assertTrue("Error: Conversion failed", Utility.existsNotEmptyAndRecent("data/tmp/Lenna5.jpg", 10000));
+			System.out.println("Conversion OK");
+		}catch(AssertionError e){
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/**
+	 * Stop services/connections.
+	 */
+	@AfterClass
+	public static void tearDown()
+	{
+		System.out.println("\n--- Stopping services ---");
+		
+		if(client != null) client.close();
+		if(drestlet != null) drestlet.stop();		
+		if(restlet != null) restlet.stop();
 	}
 }
--- src/test/java/edu/illinois/ncsa/isda/softwareserver/polyglot/PolyglotTest.java
+++ src/test/java/edu/illinois/ncsa/isda/softwareserver/polyglot/PolyglotTest.java
@@ -1,7 +1,7 @@
 package edu.illinois.ncsa.isda.softwareserver.polyglot;
 import edu.illinois.ncsa.isda.softwareserver.*;
 import kgm.utility.Utility;
-import org.junit.Test;
+import org.junit.*;
 import static org.junit.Assert.*;
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
@@ -12,62 +12,78 @@ import org.junit.runners.MethodSorters;
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PolyglotTest
-{
+{	
 	/**
-	 * Test Polyglot Server and Client.
+	 * Test conversion via client.
 	 */
 	@Test
 	public void test1()
 	{
-		System.out.println("\n=== Polyglot: Test 1 ===");
-
-		//Start Software Server    	
+		System.out.println("\n=== Polyglot: Test 1 (client) ===");
+		
+		//Start Software Server
 		SoftwareServer sserver = new SoftwareServer("SoftwareServer.conf");
-    
-    //Start Polyglot
+
+		//Start Polyglot
 		PolyglotServer pserver = new PolyglotServer("PolyglotServer.conf");
 		PolyglotClient pclient = new PolyglotClient("localhost", 50002);
 		
 		//Run the test
 		pclient.convert("data/demo/Lenna.png", "data/tmp/", "gif");
-		pclient.close();
-		pserver.stop();
-
-		assertTrue("Conversion failed", Utility.existsNotEmptyAndRecent("data/tmp/Lenna.gif", 100000));
 		
-		//Stop Software Server
-		sserver.stop();
+		try{
+			assertTrue("Error: Conversion failed", Utility.existsNotEmptyAndRecent("data/tmp/Lenna.gif", 100000));
+			System.out.println("Conversion OK");
+		}catch(AssertionError e){
+			System.out.println(e.getMessage());
+		}finally{
+			pclient.close();
+			pserver.stop();
+			sserver.stop();
+		}
 	}
 	
 	/**
-	 * Test PolyglotRestlet.
+	 * Test conversion via REST interface.
 	 */
 	@Test
 	public void test2()
 	{
-		System.out.println("\n=== Polyglot: Test 2 ===");
-
-		//Start Software Server    	
+		System.out.println("\n=== Polyglot: Test 2 (REST interface) ===");
+		
+		//Start Software Server
 		SoftwareServer sserver = new SoftwareServer("SoftwareServer.conf");
-    
-    //Start Polyglot
-		PolyglotRestlet polyglot = new PolyglotRestlet();
-		polyglot.main(new String[0]);
-		polyglot.setReturnURL(true);
-		polyglot.setSoftwareServerRESTInterface(false);
+		
+		//Start Polyglot
+		PolyglotRestlet prestlet = new PolyglotRestlet();
+		prestlet.main(new String[0]);
+		prestlet.setReturnURL(true);
+		prestlet.setSoftwareServerRESTInterface(false);
 		
 		//Run the test
 		Utility.pause(2000);
 		String result = Utility.postFile("http://localhost:8184/convert/gif/", "data/demo/Lenna.png", "text/plain");
 		Utility.pause(2000);
 		
-		assertTrue("Did not get answer from URL [" + result + "]", Utility.existsURL(result));
-				
-		Utility.downloadFile("data/tmp/", "Lenna2", result);
-		assertTrue("Conversion failed", Utility.existsNotEmptyAndRecent("data/tmp/Lenna2.gif", 10000));
+		//Check if the output endpoint is available
+		try{
+			assertTrue("Did not get answer from URL [" + result + "]", Utility.existsURL(result));
+			System.out.println("Endpoint OK");
+		}catch(AssertionError e){
+			System.out.println(e.getMessage());
+		}
 		
-		//Stop Software Server
-		polyglot.stop();
-		sserver.stop();
+		//Download the output of the conversion
+		Utility.downloadFile("data/tmp/", "Lenna2", result);
+		
+		try{
+			assertTrue("Conversion failed", Utility.existsNotEmptyAndRecent("data/tmp/Lenna2.gif", 10000));
+			System.out.println("Conversion OK");
+		}catch(AssertionError e){
+			System.out.println(e.getMessage());
+		}finally{
+			prestlet.stop();
+			sserver.stop();
+		}
 	}
 }
