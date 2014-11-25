0dfcd25ba7723cc679644c7166f4b111a1f53d6e
Kenton McHenry <kmchenry@ncsa.illinois.edu>
2014-11-25 14:47:45 -0600

    Added intial clean up steps for polyglot test.

--- src/test/java/edu/illinois/ncsa/isda/softwareserver/polyglot/PolyglotTest.java
+++ src/test/java/edu/illinois/ncsa/isda/softwareserver/polyglot/PolyglotTest.java
@@ -1,6 +1,8 @@
 package edu.illinois.ncsa.isda.softwareserver.polyglot;
 import edu.illinois.ncsa.isda.softwareserver.*;
 import kgm.utility.Utility;
+import java.lang.*;
+import java.io.*;
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.junit.FixMethodOrder;
@@ -13,6 +15,47 @@ import org.junit.runners.MethodSorters;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PolyglotTest
 {	
+	/**
+	 * Purge mongo and rabbitmq to ensure a fresh run.
+	 */
+	@BeforeClass
+	public static void setup()
+	{
+		Process p;
+		BufferedReader reader;
+		String command, line;
+
+		System.out.println("\n--- Purging mongo & rabbitmq ---");
+
+		//Purge mongo
+		try{
+			command = "mongo polyglot --eval \"db.steward.drop()\"";
+			System.out.println("> " + command);
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	
+			while((line = reader.readLine()) != null){
+				System.out.println(line);
+			}
+		}catch(Exception e) {e.printStackTrace();}
+
+		System.out.println();
+
+		//Purge rabbitmq queue
+		try{
+			command = "curl -i -u guest:guest -H \"content-type:application/json\" -XDELETE http://localhost:15672/api/queues/%2F/ImageMagick/contents";
+			System.out.println("> " + command);
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	
+			while((line = reader.readLine()) != null){
+				System.out.println(line);
+			}
+		}catch(Exception e) {e.printStackTrace();}
+	}
+		
 	/**
 	 * Test conversion via client.
 	 */
