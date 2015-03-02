308f1cabbb5f41b26e6afdd8bc6de45fa49545d7
Rob Kooper <kooper@illinois.edu>
2015-03-02 16:42:32 -0600

    reuse smtp connection

--- src/main/web/dap/tests/tests.py
+++ src/main/web/dap/tests/tests.py
@@ -89,10 +89,10 @@ def main():
 								message += report
 								message += 'Report of last run can be seen here: \n\n http://' + socket.getfqdn() + '/dap/tests/tests.php?dap=' + host + '&run=false&start=true\n'
 		
+								mailserver = smtplib.SMTP('localhost')
 								for watcher in watchers:
-									mailserver = smtplib.SMTP('localhost')
 									mailserver.sendmail('', watcher, message)
-									mailserver.quit()
+								mailserver.quit()
 
 		dt = time.time() - t0;
 		print 'Elapsed time: ' + timeToString(dt)
@@ -125,10 +125,10 @@ def main():
 				message += 'Report of last run can be seen here: \n\n http://' + socket.getfqdn() + '/dap/tests/tests.php?dap=' + host + '&run=false&start=true\n\n'
 				message += 'Elapsed time: ' + timeToString(dt)
 
+				mailserver = smtplib.SMTP('localhost')
 				for watcher in watchers:
-					mailserver = smtplib.SMTP('localhost')
 					mailserver.sendmail('', watcher, message)
-					mailserver.quit()
+				mailserver.quit()
 		else:
 			if os.path.isfile('tmp/failures.txt'):
 				#Send failure rectification emails
@@ -151,10 +151,10 @@ def main():
 						message += 'Report of last run can be seen here: \n\n http://' + socket.getfqdn() + '/dap/tests/tests.php?dap=' + host + '&run=false&start=true\n\n'
 						message += 'Elapsed time: ' + timeToString(dt)
 
+						mailserver = smtplib.SMTP('localhost')
 						for watcher in watchers:
-							mailserver = smtplib.SMTP('localhost')
 							mailserver.sendmail('', watcher, message)
-							mailserver.quit()
+						mailserver.quit()
 			else:
         #Send success notification emails
 				with open('pass_watchers.txt', 'r') as watchers_file:
@@ -169,10 +169,10 @@ def main():
 					message += 'Subject: DAP Tests Passed\n\n'
 					message += 'Elapsed time: ' + timeToString(dt)
 
+					mailserver = smtplib.SMTP('localhost')
 					for watcher in watchers:
-						mailserver = smtplib.SMTP('localhost')
 						mailserver.sendmail('', watcher, message)
-						mailserver.quit()
+					mailserver.quit()
 
 
 def convert(host, input_filename, output, output_path):
