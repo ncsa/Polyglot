dd7801a7d5c401d213114de7533d49b5e6431212
Kenton McHenry <kmchenry@ncsa.illinois.edu>
2015-03-18 11:03:27 -0500

    Simplified convert-dap.sh example.

--- src/main/scripts/convert-dap.sh
+++ src/main/scripts/convert-dap.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
+url="http://dap-dev.ncsa.illinois.edu:8184/convert/$2/"
 
-host="http://dap-dev.ncsa.illinois.edu:8184"
-input="jp2"
-output="jpg"
-
-url=$host/convert/$output/
-
-for input_file in `ls *.$input` ; do
+for input_file in `ls *.$1` ; do
 	output_url=`curl -s -H "Accept:text/plain" -F "file=@$input_file" $url`
-	output_file=${input_file%.*}.$output
+	output_file=${input_file%.*}.$2
 	echo "Converting: $input_file to $output_file"
 
 	while : ; do
 		wget -q -O $output_file $output_url
-		
-		if [ ${?} -eq 0 ] ; then
-			break
-		fi
-
+		if [ ${?} -eq 0 ] ; then break ; fi
 		sleep 1
 	done
 done
