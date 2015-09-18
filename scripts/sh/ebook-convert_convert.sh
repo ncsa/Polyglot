1efe0c1cc881ad9206107cda1dce3a732656a12b
Rui Liu <ruiliu@illinois.edu>
2015-09-18 09:40:28 -0500

    POL-165 ebook-convert supports docx, not doc.
    http://manual.calibre-ebook.com/conversion.html
    (search for "Convert Microsoft Word documents" and "Older .doc files")

--- scripts/sh/ebook-convert_convert.sh
+++ scripts/sh/ebook-convert_convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #ebook-convert (v1.31)
 #ebook
-#lrf, rar, zip, rtf, lit, txt, txtz, text, htm, xhtm, html, htmlz, xhtml, pdf, pdb, updb, pdr, prc, mobi, azw, doc, epub, fb2, djv, djvu, lrx, cbr, cbz, cbc, oebzip, rb, imp, odt, chm, tpz, azw1, pml, pmlz, mbp, tan, snb, xps, oxps, azw4, book, zbf, pobi, docm, md, textile, markdown, ibook, iba, azw3, ps
+#lrf, rar, zip, rtf, lit, txt, txtz, text, htm, xhtm, html, htmlz, xhtml, pdf, pdb, updb, pdr, prc, mobi, azw, docx, epub, fb2, djv, djvu, lrx, cbr, cbz, cbc, oebzip, rb, imp, odt, chm, tpz, azw1, pml, pmlz, mbp, tan, snb, xps, oxps, azw4, book, zbf, pobi, docm, md, textile, markdown, ibook, iba, azw3, ps
 #epub, mobi, azw3, fb2, htmlz, lit, lrf, pdb, pdf, pmlz, rb, rtf, snb, tcr, txt, txtz, zip
 
 xvfb-run ebook-convert "$1" "$2" > /dev/null
