a5013622a740a36644c49bb88f2fba06a8c3e95f
Rui Liu <ruiliu@illinois.edu>
2016-05-11 16:10:31 -0500

    POL-173 Portable purge.sh.
    A user only needs to modify one place: POL_ROOT_DIR. Tested against
    Ubuntu's plain dash and Mac's bash.

--- src/main/assembly/linux/system/purge.sh
+++ src/main/assembly/linux/system/purge.sh
@@ -1,8 +1,25 @@
+#!/bin/sh
+
 #/sbin/stop polyglot
 #/sbin/stop softwareserver
 
-find /home/polyglot/tmp/SoftwareServer/Cache -atime +1 -exec /bin/rm {} +
-find /home/polyglot/tmp/PolyglotRestlet/Public -atime +1 -exec /bin/rm {} +
+# Change this path according to this POL-SS installation.
+POL_ROOT_DIR=/home/polyglot/polyglot
+
+PO_RootPath=`grep RootPath $POL_ROOT_DIR/PolyglotRestlet.conf | cut -d= -f2 | tr -d '\r\n'`
+SS_RootPath=`grep RootPath $POL_ROOT_DIR/SoftwareServer.conf | cut -d= -f2 | tr -d '\r\n'`
+case "$PO_RootPath" in
+    /*) P1=$PO_RootPath ;;
+    *)  P1=$POL_ROOT_DIR/$PO_RootPath ;;
+esac
+case "$SS_RootPath" in
+    /*) S1=$SS_RootPath ;;
+    *)  S1=$POL_ROOT_DIR/$SS_RootPath ;;
+esac
+
+# Purge.
+find $P1/Public -atime +1 -exec /bin/rm {} +
+find $S1/Cache -atime +1 -exec /bin/rm {} +
 
 #/sbin/start softwareserver
 #/sbin/start polyglot
