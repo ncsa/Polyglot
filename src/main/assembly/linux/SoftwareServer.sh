aa1e5521a92f837a95388b663c709c0da14d8ba8
Kenton McHenry <mchenry@illiois.edu>
2016-10-13 23:23:48 -0500

    Modified packaged script to take multiple params.

--- src/main/assembly/linux/SoftwareServer.sh
+++ src/main/assembly/linux/SoftwareServer.sh
@@ -1,1 +1,1 @@
-java -cp polyglot.jar:lib/* -Xmx1g edu.illinois.ncsa.isda.softwareserver.SoftwareServer $1 $2
+java -cp polyglot.jar:lib/* -Xmx1g edu.illinois.ncsa.isda.softwareserver.SoftwareServer $@
