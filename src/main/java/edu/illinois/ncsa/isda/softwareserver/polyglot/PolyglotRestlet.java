96d8a2ac6f3b7d825908718ff9432965a43f2d8c
bing zhang <bing@illinois.edu>
2017-03-21 16:13:15 -0500

    replace question marks as underscores

--- src/main/java/edu/illinois/ncsa/isda/softwareserver/polyglot/PolyglotRestlet.java
+++ src/main/java/edu/illinois/ncsa/isda/softwareserver/polyglot/PolyglotRestlet.java
@@ -333,6 +333,7 @@ public class PolyglotRestlet extends ServerResource
 			}
 		}else if(part0.equals("file")){
 			if(!part1.isEmpty()){
+				part1 = part1.replace("?","_");
 				part1 = SoftwareServerRESTUtilities.removeParameters(part1);
 				file = public_path + part1;
 
@@ -721,7 +722,7 @@ public class PolyglotRestlet extends ServerResource
 									continue;
 								}
 							}else if(HOST_POSTED_FILES){
-								file = public_path + (fi.getName()).replace(" ","_");
+								file = public_path + (fi.getName()).replace(" ","_").replace("?", "_");
 								fi.write(new File(file));
 
 								String extension = Utility.getFilenameExtension(fi.getName());
